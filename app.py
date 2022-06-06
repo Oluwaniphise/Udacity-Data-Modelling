04a7bd1d874eca47214e8fa4cf9b85d98c00bedb
Oluwaniphise <oduyaleenoch@gmail.com>
2022-06-06 15:11:13 +0100

    initial commit

--- app.py
+++ app.py
@@ -84,8 +84,8 @@ class Artist(db.Model):
 class Show(db.Model):
   __tablename__ = 'Show'
   id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
   start_time = db.Column(db.String(120))
 
   
